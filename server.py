7505618508ea28c29b221ca6db13bf39028f4411
EaeDave <EaeDav3@gmail.com>
2024-10-14 20:29:37 -0300

    profile route test

--- server.py
+++ server.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")  # @ decorator
+@app.get("/")  # @ decorator router
 async def root():
     return {"message": "this is my simple FastAPI init"}
+
+
+@app.get("/profile")  # another decorator router
+async def profile():
+    return {"name": "David Rodrigues da Silva"}
     
