63bed13f9cfca2d5708c41737d2c752cde8ee36c
EaeDave <EaeDav3@gmail.com>
2024-10-14 22:46:50 -0300

    GET method, route params and query params

--- server.py
+++ server.py
@@ -11,4 +11,24 @@ async def root():
 @app.get("/profile")  # another decorator router
 async def profile():
     return {"name": "David Rodrigues da Silva"}
-    
+
+
+@app.post("/profile")  # endpoint ou route
+async def signup():
+    return {"message": "Perfil criado com sucesso!"}
+
+
+@app.put("/profile")
+async def update():
+    return {"message": "Perfil atualizado com sucesso!"}
+
+
+@app.delete("/profile")
+async def delete():
+    return {"message": "Perfil deletado com sucesso!"}
+
+
+@app.get("/greetings/{nome}/")
+async def greetings(nome: str):
+    texto = f'Olá, {nome.capitalize()}, seja bem vindo!'
+    return {"message": texto}
