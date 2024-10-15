5e8e2136cd57428ef764002fb76a7cabb884c500
EaeDave <EaeDav3@gmail.com>
2024-10-14 23:27:56 -0300

    GET method, route params and query params - pt2

--- server.py
+++ server.py
@@ -32,3 +32,21 @@ async def delete():
 async def greetings(nome: str):
     texto = f'Olá, {nome.capitalize()}, seja bem vindo!'
     return {"message": texto}
+
+
+@app.get("/quadrado/{numero}")  # route param
+async def quadrado(numero:int):
+    resultado = numero * numero
+    return {"Message": f"O quadrado do número {numero} é = {resultado}"}
+
+
+@app.get("/dobro")  # query string  localhost:8000/dobro?valor=8
+async def dobro(value: int):
+    dobro = f"O dobro de {value} é = {value * 2}"
+    return {"message": dobro}
+
+
+@app.get("/matematica")  # query string localhost:8000/matematica?num_1=5&num_2=5
+async def somar(num_1:int = 0, num_2:int = 0):  # 0 now is a placeholder, a default value to param
+    soma = num_1 + num_2
+    return {"soma": f"A soma entre {num_1} + {num_2} é = {soma}"}
